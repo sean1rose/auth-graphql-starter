516abf940155ef24f687e5d90f605f148689b928
Sean Rose <sean1rose@gmail.com>
2017-04-28 22:31:30 +0900

    set up mlab config

--- server/server.js
+++ server/server.js
@@ -7,12 +7,13 @@ const passport = require('passport');
 const passportConfig = require('./services/auth');
 const MongoStore = require('connect-mongo')(session);
 const schema = require('./schema/schema');
+const config = require('../config');
 
 // Create a new Express application
 const app = express();
 
 // Replace with your mongoLab URI
-const MONGO_URI = '';
+const MONGO_URI = `mongodb://${config.user}:${config.pass}@ds123351.mlab.com:23351/auth`;
 
 // Mongoose's built in promise library is deprecated, replace it with ES2015 Promise
 mongoose.Promise = global.Promise;
